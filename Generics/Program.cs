5bd93ab9454589d026617980b77aeb5cd9f9e04b
Sushil <skshinde2028@gmail.com>
2023-04-29 16:16:23 +0530

    [Sushil] Print array without generic

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -10,6 +10,22 @@ namespace Generics
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the Generic concept impimentation program");
+            Console.WriteLine("Choose from the below options");
+            Console.WriteLine("1.Print Array\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            int[] intArray = { 10, 20, 30, 40 };
+            double[] doubleArray = { 10.20, 20.30, 30.40 };
+            char[] charArray = { 'a', 'b', 'c', 'd' };
+            switch (option) 
+            {
+                case 1:
+                    PrintWithoutGenerics.ToPrint(intArray);
+                    PrintWithoutGenerics.ToPrint(doubleArray);
+                    PrintWithoutGenerics.ToPrint(charArray);
+                    break;
+            }
         }
     }
 }
+
