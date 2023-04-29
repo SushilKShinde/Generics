582d5c6606e168dc8a9d8d10e419b1abe2c495de
Sushil <skshinde2028@gmail.com>
2023-04-29 17:32:33 +0530

    [Sushil] Print array using generic class

---
+++ Generics/GenericClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class GenericClass<E>
+    {
+        public static void ToPrint(E[] array)
+        {
+            Console.WriteLine("\nArray elements are: ");
+            foreach(E element in array)
+            {
+                Console.Write(element+" ");
+            }
+        }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -12,7 +12,7 @@ namespace Generics
         {
             Console.WriteLine("Welcome to the Generic concept impimentation program");
             Console.WriteLine("Choose from the below options");
-            Console.WriteLine("1.Print Array\n2.Print using generic method\n");
+            Console.WriteLine("1.Print Array\n2.Print using generic method\n3.Print using generic class\n");
             int option = Convert.ToInt32(Console.ReadLine());
             int[] intArray = { 10, 20, 30, 40 };
             double[] doubleArray = { 10.20, 20.30, 30.40 };
@@ -20,15 +20,21 @@ namespace Generics
             switch (option) 
             {
                 case 1:
-                    PrintWithoutGenerics.ToPrint(intArray);
-                    PrintWithoutGenerics.ToPrint(doubleArray);
-                    PrintWithoutGenerics.ToPrint(charArray);
+                    //PrintWithoutGenerics.ToPrint(intArray);
+                    //PrintWithoutGenerics.ToPrint(doubleArray);
+                    //PrintWithoutGenerics.ToPrint(charArray);
                     break;
                 case 2:
                     //mention data type for argument after method name(not in argument bracket)
-                    GenericMethod.ToPrint<int>(intArray);
-                    GenericMethod.ToPrint<double>(doubleArray);
-                    GenericMethod.ToPrint<char>(charArray);
+                    //GenericMethod.ToPrint<int>(intArray);
+                    //GenericMethod.ToPrint<double>(doubleArray);
+                    //GenericMethod.ToPrint<char>(charArray);
+                    break;
+                case 3:
+                    //while calling generic class mention data type for argument after class name
+                    GenericClass<int>.ToPrint(intArray);
+                    GenericClass<double>.ToPrint(doubleArray);
+                    GenericClass<char>.ToPrint(charArray);
                     break;
                 default:
                     Console.WriteLine("Choose correct option");
