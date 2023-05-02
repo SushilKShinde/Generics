7bbef7292d9ebef9658dc850543929b2059f775b
Sushil <skshinde2028@gmail.com>
2023-05-02 18:50:47 +0530

    [Sushil] Finding maximum of three

---
+++ Generics/FindMax.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class FindMax
+    {
+        public static void FindMaximum(int first, int second, int third)
+        {
+            if (first.CompareTo(second) >= 0 && first.CompareTo(third) >= 0)
+                Console.WriteLine($"{first} is bigger amoung {first} {second} {third}");
+            else if (second.CompareTo(first) >= 0 && second.CompareTo(third) >= 0)
+                Console.WriteLine($"{second} is bigger amoung {first} {second} {third}");
+            else
+                Console.WriteLine($"{third} is bigger amoung {first} {second} {third} ");
+            Console.ReadLine();
+        }
+        public static void FindMaximum (float first, float second, float third)
+        {
+            if (first.CompareTo(second) >= 0 && first.CompareTo(third) >= 0)
+                Console.WriteLine($"{first} is bigger amoung {first} {second} {third}");
+            else if (second.CompareTo(first) >= 0 && second.CompareTo(third) >= 0)
+                Console.WriteLine($"{second} is bigger amoung {first} {second} {third}");
+            else
+                Console.WriteLine($"{third} is bigger amoung {first} {second} {third} ");
+            Console.ReadLine();
+        }
+
+        public static void FindMaximum(string first, string second, string third)
+        {
+            if (first.CompareTo(second) >= 0 && first.CompareTo(third) >= 0)
+                Console.WriteLine($"{first} is bigger amoung {first} {second} {third}");
+            else if (second.CompareTo(first) >= 0 && second.CompareTo(third) >= 0)
+                Console.WriteLine($"{second} is bigger amoung {first} {second} {third}");
+            else
+                Console.WriteLine($"{third} is bigger amoung {first} {second} {third} ");
+            Console.ReadLine();
+        }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -11,15 +11,47 @@ namespace Generics
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Generic concept impimentation program");
-           
-            int[] intArray = { 10, 20, 30, 40 };
-            double[] doubleArray = { 10.20, 20.30, 30.40 };
-            char[] charArray = { 'a', 'b', 'c', 'd' };
-            //data type after class name and after method name is required
-            PrintArrayUsingGenericClassAndMethod<int>.ToPrint<int>(intArray);
-            PrintArrayUsingGenericClassAndMethod<double>.ToPrint<double>(doubleArray);
-            PrintArrayUsingGenericClassAndMethod<char>.ToPrint<char>(charArray);
-            Console.ReadLine();
+            Console.WriteLine("Program to find maximum int/float/dstring");
+            Console.WriteLine("Choose the option");
+            Console.WriteLine("\n1.Find Max integere\n2.Find Max float\n3.Find Max string");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter first integer");
+                    int first = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter second integer");
+                    int second = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter third integer");
+                    int third = Convert.ToInt32(Console.ReadLine());
+                    //call method for max integer 
+                    FindMax.FindMaximum(first, second, third);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter first float number");
+                    float first_float = Convert.ToSingle(Console.ReadLine());
+                    Console.WriteLine("Enter second float number");
+                    float second_float = Convert.ToSingle(Console.ReadLine());
+                    Console.WriteLine("Enter third float number");
+                    float third_float = Convert.ToSingle(Console.ReadLine());
+                    //call method for max float number
+                    FindMax.FindMaximum(first_float, second_float, third_float);
+                    break;
+                case 3:
+                    Console.WriteLine("Enter first string");
+                    string first_string = Console.ReadLine();
+                    Console.WriteLine("Enter second string");
+                    string second_string = Console.ReadLine();
+                    Console.WriteLine("Enter third string");
+                    string third_string = Console.ReadLine();
+                    //call method for max float number
+                    FindMax.FindMaximum(first_string, second_string, third_string);
+                    break;
+                default:
+                    Console.WriteLine("Choose correct option");
+                    break;
+            }
         }
     }
 }
